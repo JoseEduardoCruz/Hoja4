84d06779100b9e0d6f49ff38d97fc18fcf68308e
Jose <Jose@Jose>
2014-08-19 08:40:29 -0500

    new commit

---
+++ src/hoja/de/trabajo4/Pila.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hoja.de.trabajo4;
+
+/**
+ *
+ * @author Jose
+ */
+public class Pila <T> {
+    public T valor;
+    
+    abstract public void push();    
+    
+    abstract public T pop();
+    
+}
